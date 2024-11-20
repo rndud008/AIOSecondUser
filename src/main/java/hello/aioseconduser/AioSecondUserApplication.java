ace6b5766f1e189f33d3f4605ce06fa576bec318
rndud008 <rndud008@gamil.com>
2024-11-20 17:04:01 +0900

    init commit

---
+++ src/main/java/hello/aioseconduser/AioSecondUserApplication.java
@@ -0,0 +1,13 @@
+package hello.aioseconduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AioSecondUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AioSecondUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/aioseconduser/AioSecondUserApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.aioseconduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AioSecondUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
